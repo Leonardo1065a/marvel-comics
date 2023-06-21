91f42fdf2d2e685ade1f826c17b9658c1bb932a6
Leonardo Silva <leonardoplxy@gmail.com>
2023-06-20 21:18:05 -0300

    added generic service store

--- src/app/core/services/commons.service.ts
+++ src/app/core/services/commons.service.ts
@@ -2,6 +2,7 @@ import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+
 export abstract class CommonsService<T> {
   protected __store = inject(Store);
 
